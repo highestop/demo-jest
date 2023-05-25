a4ecdb4c793c2bd74587237efcee638c6e98138d
Highestop Chen <highestopchen@gmail.com>
2023-05-25 23:25:33 +0800

    jest setup files

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,7 @@
 export default {
   testEnvironment: 'node',
   preset: 'ts-jest',
+  rootDir: '',
+  setupFiles: [`<rootDir>/setup.ts`],
+  setupFilesAfterEnv: [`<rootDir>/setup-after-env.ts`],
 };
---
+++ setup-after-env.ts
@@ -0,0 +1,19 @@
+import { stringify } from 'ts-jest';
+
+declare global {
+  const SCENARIO: jest.Describe;
+  const GIVEN: jest.Describe;
+  const WHEN: jest.Describe;
+  const THEN: jest.It;
+}
+
+const globalAny: any = global;
+
+globalAny.SCENARIO = (name: string, fn: () => void) =>
+  describe(`SCENARIO: ${stringify(name)}`, fn);
+globalAny.GIVEN = (name: string, fn: () => void) =>
+  describe(`GIVEN: ${stringify(name)}`, fn);
+globalAny.WHEN = (name: string, fn: () => void) =>
+  describe(`WHEN: ${stringify(name)}`, fn);
+globalAny.THEN = (name: string, fn: () => void) =>
+  test(`THEN: ${stringify(name)}`, fn);

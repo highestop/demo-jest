8e58038fc2534f73d16cd5000fe13dcbf5ce8f23
Highestop Chen <highestopchen@gmail.com>
2023-05-25 23:46:27 +0800

    mock and report

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,42 @@
+import type { RawCompilerOptions } from 'ts-jest';
+import { pathsToModuleNameMapper } from 'ts-jest';
+import tsconfig from './tsconfig.base.json';
+
 export default {
   testEnvironment: 'node',
   preset: 'ts-jest',
   rootDir: '',
   setupFiles: [`<rootDir>/setup.ts`],
   setupFilesAfterEnv: [`<rootDir>/setup-after-env.ts`],
+  testMatch: [`<rootDir>/src/**/*.(test|spec).(ts|tsx)`],
+  transform: {
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        isolatedModules: true,
+        tsconfig: tsconfig.compilerOptions as RawCompilerOptions,
+      },
+    ],
+    '^.+\\.(css|less|sass|scss|svg)$': 'jest-transform-stub',
+  },
+  // https://kulshekhar.github.io/ts-jest/docs/getting-started/paths-mapping/
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': '<rootDir>/node_modules/jest-css-modules',
+    '\\.svg$': `<rootDir>/svgr-mock.ts`,
+    '.*\\?worker': `<rootDir>/worker-mock.ts`,
+    // https://huafu.github.io/ts-jest/user/config/
+    ...pathsToModuleNameMapper(tsconfig.compilerOptions.paths),
+  },
+  testResultsProcessor: 'jest-junit',
+  collectCoverageFrom: [
+    `<rootDir>/src/**/*.(ts|tsx)`,
+    '!**/__tests__/**',
+    '!**/__spec__/**',
+    '!**/*.(test|spec).(ts|tsx)',
+  ],
+  // https://gist.github.com/rishitells/3c4536131819cff4eba2c8ab5bbb4570
+  coverageReporters: ['cobertura', 'text'],
+  coverageDirectory: 'out/Test',
+  // https://github.com/jest-community/jest-junit
+  reporters: ['default', 'jest-junit'],
 };
--- src/index.spec.ts
+++
@@ -1,5 +0,0 @@
-describe("test", () => {
-    it("expect", () => {
-        expect(1).toBeTruthy()
-    })
-})
